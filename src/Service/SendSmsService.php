1ae90cf0fbf7551cc0e4d5bcb16621442b89585a
alexander pekh <sashapekh@gmail.com>
2021-01-31 21:10:00 +0200

    add multiple send sms

--- src/Service/SendSmsService.php
+++ src/Service/SendSmsService.php
@@ -2,27 +2,25 @@
 
 namespace Sashapekh\VfApi;
 
+use Psr\Http\Message\ResponseInterface;
+
 class SendSmsService
 {
     private $tokenService;
     private $sendSmsUri;
     private $mainDomain;
+    private $sendMultipleSmsUri;
 
     public function __construct()
     {
         $this->mainDomain = config('vf-service-variables.mainDomain');
         $this->tokenService = new TokenService();
         $this->sendSmsUri = config('vf-service-variables.SendSmsUri');
+        $this->sendMultipleSmsUri = config('vf-service-variables.SendMultipleUri');
     }
 
     public function sendOneSms(string $phone, string $message)
     {
-        $header = [
-            'Accept' => 'application/json',
-            'Authorization' => 'Bearer ' . $this->tokenService->getToken()
-
-        ];
-
         $jsonData = [
             'phone' => $phone,
             'message' => $message
@@ -30,14 +28,47 @@ class SendSmsService
 
         $response = (new CurlSenderService(
             $this->sendSmsUri,
-            $header,
+            $this->setHeaders(),
             $jsonData
         ))->sendPostCurl();
 
-        if (isset($response)) {
-            return json_decode($response->getBody()->getContents(), true);
-        }
-        return null;
+        return $this->getProcessedResponse($response);
 
+    }
+
+    public function sendMultipleSms(array $phones = [], string $message = '')
+    {
+        if (empty($phones) && empty($message)) {
+            return null;
+        }
+
+        $jsonData = [
+            'phone' => $phones,
+            'message' => $message
+        ];
+
+        $response = (new CurlSenderService(
+            $this->sendMultipleSmsUri,
+            $this->setHeaders(),
+            $jsonData
+        ))->sendPostCurl();
+
+        return $this->getProcessedResponse($response);
+    }
+
+    private function setHeaders(): array
+    {
+        return [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $this->tokenService->getToken()
+
+        ];
+    }
+
+    private function getProcessedResponse($response)
+    {
+        return isset($response)
+            ? json_decode($response->getBody()->getContents(), true)
+            : null;
     }
 }
